076fddbfba8c39b77b4ecc773bca422075ba9080
akinniyi1 <64546693+akinniyi1@users.noreply.github.com>
2025-06-23 21:08:27 +0100

    Create src-App.jsx

---
+++ src-App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white text-gray-900 font-sans">
+      <h1 className="text-2xl font-bold">PDF Toolbox Web App</h1>
+    </div>
+  );
+}
+
+export default App;
