5e4f85a77920e7d10a768dd2f2720c60e51f229d
akinniyi1 <64546693+akinniyi1@users.noreply.github.com>
2025-06-25 10:47:47 +0100

    Update ton.js

--- src/lib/ton.js
+++ src/lib/ton.js
@@ -1,6 +1,2 @@
-// src/lib/ton.js
-import { TonConnect } from "@tonconnect/sdk";
-
-export const tonConnect = new TonConnect({
-  manifestUrl: "https://pdf-toolbox-client.onrender.com/tonconnect-manifest.json"
-});
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+export default TonConnectUIProvider;
