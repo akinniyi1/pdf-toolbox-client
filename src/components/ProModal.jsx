4a6e1ada4c256fc12795560b09a3a0ab4c303d84
akinniyi1 <64546693+akinniyi1@users.noreply.github.com>
2025-06-29 10:56:16 +0100

    Update ProModal.jsx

--- src/components/ProModal.jsx
+++ src/components/ProModal.jsx
@@ -1,23 +1,11 @@
 import React from "react";
-import axios from "axios";
 
 function ProModal({ onClose, onUpgrade }) {
-  const telegramUserId = window.Telegram?.WebApp?.initDataUnsafe?.user?.id;
-
-  const handleUpgrade = async () => {
+  const handleUpgrade = () => {
     const walletLink = `https://app.tonkeeper.com/transfer/UQD-iJ1whFaOz-42NRmJPJ9U7bKAjsXgPiaY-cqRiHeq8AKs?amount=500000000&text=PDF%20Toolbox%20Pro%20Upgrade`;
     window.open(walletLink, "_blank");
-
-    // Wait 3 seconds, then mark as upgraded in backend
-    setTimeout(async () => {
-      if (telegramUserId) {
-        await axios.post(`https://pdf-toolbox-server.onrender.com/user/${telegramUserId}`, {
-          pro: true,
-        });
-      }
-      localStorage.setItem("pdfToolboxPro", "1");
-      onUpgrade();
-    }, 3000);
+    // Assume payment made, mark as Pro
+    onUpgrade();
   };
 
   return (
