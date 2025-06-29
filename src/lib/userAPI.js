a55ae5260e9697d59af547e0ec5c9593667a4f56
akinniyi1 <64546693+akinniyi1@users.noreply.github.com>
2025-06-29 12:32:05 +0100

    Update userAPI.js

--- src/lib/userAPI.js
+++ src/lib/userAPI.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = "https://pdf-toolbox-server.onrender.com";
+const BASE = "https://pdf-toolbox-server.onrender.com";
 
 export async function getUserData(id) {
-  const res = await axios.get(`${BASE_URL}/user/${id}`);
+  const res = await axios.get(`${BASE}/user/${id}`);
   return res.data;
 }
 
 export async function updateUserData(id, data) {
-  const res = await axios.post(`${BASE_URL}/user/${id}`, data);
+  const res = await axios.post(`${BASE}/user/${id}`, data);
   return res.data;
 }
