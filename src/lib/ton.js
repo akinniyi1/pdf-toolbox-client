eec1ae18d4d17b0723f41d614f1d07deba463db6
akinniyi1 <64546693+akinniyi1@users.noreply.github.com>
2025-07-04 11:37:52 +0100

    Delete src/lib/ton.js

--- src/lib/ton.js
+++
@@ -1,5 +0,0 @@
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
-
-const manifestUrl = 'https://pdf-toolbox-client.onrender.com/tonconnect-manifest.json';
-
-export { TonConnectUIProvider, manifestUrl };
